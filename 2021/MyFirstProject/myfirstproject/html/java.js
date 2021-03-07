2e61cc4c089b6f9d1043c54f611df83ce73efe4e
shoxruxnuriddinov <shoxanuriddinov0@gmail.com>
2021-03-07 14:13:44 +0500

    go save

---
+++ 2021/MyFirstProject/myfirstproject/html/java.js
@@ -0,0 +1,45 @@
+
+alert('BLΛƆKPIИK')
+const group="BLΛƆKPIИK"
+console.log(group)
+const theyare=[
+                {firstname: "Roseanne Park",
+                    mononym: "Rose",
+                    country: "New Zealand",
+                    year:1997,
+                    month: "February",
+                    date: 11,
+                    age: 24,
+                    occuption: "singer"
+                },
+                { firstname: "Kim Ji-soo",
+                    mononym: "Jisoo",
+                    country: "South Korea",
+                    year: 1995,
+                    month:"Januray",
+                  date: 3,
+                  age: 26,
+                  occuption: "singer-actress",
+                },
+                { firstname: "Lalisa Manoban",
+                  mononym: "Lisa",
+                  country: "Thailand",
+                  year: 1997,
+                  month: "March",
+                  date: 27,
+                  age: 23,
+                  occuption:"Reper-singer-dancer",
+                },
+                { firstname: "Kim Jennie",
+                  mononym: "Jennie",
+                  country: "South Korea",
+                  year: 1996,
+                  month: "Januray",
+                  date: 16,
+                  age: 25,
+                  occuption:"",
+                },
+                ]
+console.log(theyare);
+
+
